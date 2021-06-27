0a7e37c3e0e717e89fbeb9ebfb1fba720fb78db0
piyaakotharii <19bcs3544@cuchd.in>
2021-06-27 12:17:42 +0530

    created input search bar and fetched results from tv  maze api

--- src/components/Navs.js
+++ src/components/Navs.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const LINKS = [
-    {to: '/', text: 'HomePage'},
-    {to: '/starred', text: 'StarredPage'}
-]
+  { to: '/', text: 'Home' },
+  { to: '/starred', text: 'Starred' },
+];
 
 const Navs = () => {
-    return (
-        <div>
-            <ul>  
-                {LINKS.map(item => 
-                 ( <li key={item.to}>
-                  <Link   to={item.to} >  {item.text}  </Link>  </li> ))}  
-           </ul>  
-       </div>
-    )
-}
+  return (
+    <div>
+      <ul>
+        {LINKS.map(item => (
+          <li key={item.to}>
+            <Link to={item.to}>{item.text}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default Navs
+export default Navs;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,40 @@
-import React from 'react'
-import MainPageLayout from '../components/MainPageLayout'
+import React, { useState } from 'react';
+import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
-    return (
-        <MainPageLayout>
-            This is Home page.
-        </MainPageLayout>
-    )
-}
+  const [input, setInput] = useState('');
 
-export default Home
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
+};
+
+export default Home;
