35edd444c943d2a9a62e2be56f5bcfdc420b7648
piyaakotharii <19bcs3544@cuchd.in>
2021-06-30 11:01:49 +0530

    added data to show page components/seasons error fixed.

--- src/components/show/ShowCard.styled.js
+++ src/components/show/ShowCard.styled.js
@@ -1,34 +1,33 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 import { SearchCard } from '../styled';
 
-
 export const StyledShowCard = styled(SearchCard)`
-.btns {
-  margin-top: 15px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  a {
-    text-decoration-color: #000;
-    color: #000;
-    &:hover {
-      text-decoration-color: blue;
-      color: blue;
-    }
-  }
-  button {
-    outline: none;
-    border: 1px solid #8e8e8e;
-    border-radius: 15px;
-    padding: 5px 20px;
-    background-color: #fff;
+  .btns {
+    margin-top: 15px;
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
-    &:hover {
-      cursor: pointer;
+    a {
+      text-decoration-color: #000;
+      color: #000;
+      &:hover {
+        text-decoration-color: blue;
+        color: blue;
+      }
+    }
+    button {
+      outline: none;
+      border: 1px solid #8e8e8e;
+      border-radius: 15px;
+      padding: 5px 20px;
+      background-color: #fff;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      &:hover {
+        cursor: pointer;
+      }
     }
   }
-}
 `;
